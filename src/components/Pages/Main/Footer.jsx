284abf0bd42ae79c1670dc209ad63658dcec6519
The Zenxx <94261435+zenxxdev@users.noreply.github.com>
2024-08-18 10:44:18 +0300

    Update Footer.jsx

--- src/components/Pages/Main/Footer.jsx
+++ src/components/Pages/Main/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
     return (
         <div>
             <div className='flex justify-center sm:justify-between mx-0 sm:mx-24 py-4 sm:py-4'>
-                <p className='hidden sm:block'>Copyright © 2024 Lourity</p>
-                <p>Developed with by <span className='text-sky-500 font-semibold'>Lourity</span></p>
+                <p className='hidden sm:block'>Copyright © 2024 Zenxx</p>
+                <p>Developed with by <span className='text-sky-500 font-semibold'>Zenxx</span></p>
             </div>
             <span className="shadow-[0_0_1000px_40px_#0ea5e9]"></span>
         </div>
